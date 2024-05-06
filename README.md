752abff93f1e585c596661653ddd541c186f164a
flan02 <chanivetdan@hotmail.com>
2024-05-06 11:43:31 -0300

    Started code installed.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nextauth-v5
-Every new feature  in nextauth 2024
+Every new feature  in next Auth v5 (2024)
+
+# Subjects
+
+- [ ] OAuth(Google, Github, etc)
+- [ ] Email magic links (via Resend)
+- [ ] Protect pages, server actions, route handlers
+- [ ] Server-side vs client-side authentication, caching
+- [ ] Roles (e.g. admin, user)
+- [ ] Update user data
---
+++ src/lib/mongodb.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const { MONGODB_URI } = process.env
+
+if (!MONGODB_URI) {
+  throw new Error("No mongo connection string. Set MONGODB_URI environment variable.");
+}
+
+let isConnected: boolean = false
+
+export const connectDB = async () => {
+  if (!isConnected) {
+    try {
+      const client = await mongoose.connect(MONGODB_URI)
+      if (client.connection.readyState === 1) console.log("Connected to MongoDB")
+      console.log("New connection to mongodb atlas cloud");
+      isConnected = true
+      return Promise.resolve(true)
+    } catch (error) {
+      console.log("Error connecting to MongoDB: ", error)
+      return Promise.resolve(false)
+    }
+  }
+  else console.log("Using existing connection to database")
+}
+
+
+
